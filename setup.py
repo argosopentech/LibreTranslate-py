a84c8d198f2c2c62f63c2f1142eb968e7937c9ef
P.J. Finlay <pjappdevelopment@gmail.com>
2021-05-30 18:22:18 -0500

    Packaged

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+        name='libretranslatepy',
+        version='1.0',
+        description='Python bindings for LibreTranslate API',
+        url='https://www.argosopentech.com',
+        packages=find_packages()
+)

8aa7f684391a127f227b0fc9a697082f550af1ed
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2021-06-16 13:02:35 -0400

    Update setup.py
    * long_description variable defined but never used.
    * url should point to the project repository or to the official page.
    * Add the minimum supported version of Python, calculated with https://github.com/netromdk/vermin
    * Add more options

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open('README.md') as f:
     long_description = f.read()
 
-setup(
-        name='libretranslatepy',
-        version='2.1.0',
-        description='Python bindings for LibreTranslate API',
-        url='https://www.argosopentech.com',
-        packages=find_packages()
+setup(name='libretranslatepy',
+      version='2.1.0',
+      description='Python bindings for LibreTranslate API',
+      long_description=long_description,
+      keywords="translation libretranslate",
+      url='https://github.com/argosopentech/LibreTranslate-py',
+      python_requires=">=2.6",
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2"
+        "Programming Language :: Python :: 3",
+        ],
+      project_urls={
+        "Bug Reports": "https://github.com/argosopentech/LibreTranslate-py/issues",
+        "Source": "https://github.com/argosopentech/LibreTranslate-py/",
+        },
+      packages=find_packages()
 )

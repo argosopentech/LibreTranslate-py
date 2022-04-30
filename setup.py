bc6f6355f99ff481947ca312b9a7e923f2eefd8b
P.J. Finlay <pjappdevelopment@gmail.com>
2022-04-30 06:52:02 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 from setuptools import find_packages, setup
 
-with open('README.md') as f:
-    long_description = f.read()
-
 setup(name='libretranslatepy',
-      version='2.1.0',
+      version='2.1.2',
       description='Python bindings for LibreTranslate API',
-      long_description=long_description,
       keywords="translation libretranslate",
       url='https://github.com/argosopentech/LibreTranslate-py',
       python_requires=">=2.6",
@@ -15,7 +11,7 @@ setup(name='libretranslatepy',
         "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2"
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         ],
       project_urls={
